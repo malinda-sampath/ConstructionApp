a6563bd447db3db22d8c9715c052500c1cee1fc2
laikalakmal <lakmallaika@gmail.com>
2024-08-22 10:17:34 +0530

    connecting string is added to the App.config

---
+++ Models/Vehical.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConstructionApp.Models
+{
+    internal abstract class Vehical
+    {
+        private string regNo;
+        private string fuelType;
+        private string model;
+        private DateOnly licenseRenewDate;
+        private decimal fuelUsage;
+
+        public string RegNo
+        {
+            get => regNo;
+            set => regNo = value;
+        }
+
+        public string FuelType
+        {
+            get => fuelType;
+            set
+            {
+                if (value == "Petrol" || value == "Diesel")
+                {
+                    fuelType = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Fuel type must be either 'Petrol' or 'Diesel'.");
+                }
+            }
+        }
+
+        public string Model1
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public DateOnly LicRenewDate
+        {
+            get => licenseRenewDate;
+            set => licenseRenewDate = value;
+        }
+
+        public decimal FuelUsage
+        {
+            get => fuelUsage;
+            set => fuelUsage = value;
+        }
+    }
+}
